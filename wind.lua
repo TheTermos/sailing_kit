3cbd0009c4bf9ca3e7ab555c52d88bdcddf38b53
TheTermos <55103816+TheTermos@users.noreply.github.com>
2019-10-17 14:21:58 +0200

    Update wind.lua

--- wind.lua
+++ wind.lua
@@ -2,7 +2,7 @@ local yaw = math.random()*math.pi*2-math.pi
 wind={}
 wind.wind = vector.multiply(minetest.yaw_to_dir(yaw),10)
 wind.timer = 0
-wind.ttime = math.random()*50+10
+wind.ttime = math.random()*5*60+1*60
 
 minetest.register_globalstep(
 	function(dtime)
@@ -11,6 +11,6 @@ minetest.register_globalstep(
 			local yaw = minetest.dir_to_yaw(wind.wind)
 			local yaw = yaw+math.random()-0.5
 			wind.wind = vector.multiply(minetest.yaw_to_dir(yaw),10)
-			wind.ttime = wind.timer+math.random()*50+10
+			wind.ttime = wind.timer+math.random()*5*60+1*60
 		end
 	end)
